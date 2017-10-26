371efd1f7b3ae1ed9a7d3eeccaee8f23e799afa2
Dani <danihazler@gmail.com>
2017-10-26 14:46:37 +0100

    Add jQuery, organise old file

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  //  ------- NAVIGATION SCROLL ------
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  //  ----------- SCROLL TO THE TOP ------------
+  $(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+});
